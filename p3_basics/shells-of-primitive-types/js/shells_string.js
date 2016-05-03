fab54be6c94a7430ae545d3d1b4f02ec574691e9
shackijj <shaackiy@gmail.com>
2016-05-04 00:16:05 +0300

    More string methods

--- p3_basics/shells-of-primitive-types/js/shells_string.js
+++ p3_basics/shells-of-primitive-types/js/shells_string.js
@@ -21,3 +21,53 @@ log(result);
 
 result = val.concat("world", "!");
 log(result);
+
+log(stringValue.slice(3));
+log(stringValue.substring(3));
+log(stringValue.substr(3));
+log(stringValue.slice(3, 7));
+log(stringValue.substring(3, 7));
+log(stringValue.substr(3, 7));
+
+log("Negative indexes");
+
+log(stringValue.slice(-3));
+log(stringValue.substring(-3));
+log(stringValue.substr(-3));
+log(stringValue.slice(3, -4));
+log(stringValue.substring(3, -4));
+log(stringValue.substr(3, -4));
+
+log("Searching");
+
+log(stringValue.indexOf("o"));
+log(stringValue.lastIndexOf("o"));
+
+log(stringValue.indexOf("o", 6));
+log(stringValue.lastIndexOf("o", 6));
+
+var text = "Lorem ipsum dolor sit amet, consectetur adipisocong...";
+var pos = text.indexOf("o");
+var positions = [];
+
+while(pos > -1) {
+    positions.push(pos);
+    pos = text.indexOf("o", pos + 1);
+}
+log(positions);
+
+// ECMAScript5 trim
+var beforeTrim = " Yellow world ";
+log("beforeTrim : '" + beforeTrim + "'"); 
+
+var afterTrim = beforeTrim.trim();
+log("afterTrim: '" + afterTrim + "'");
+log("beforeTrim again: '" + beforeTrim + "'");
+
+log("Registry-changing methods");
+
+var mixedRegistryStr = "KaWaBaNgA";
+log(".toLowerCase " + mixedRegistryStr.toLowerCase());
+log(".toLocaleLowerCase " + mixedRegistryStr.toLocaleLowerCase());
+log(".toUpperCase " + mixedRegistryStr.toUpperCase());
+log(".toLocaleUpperCase " + mixedRegistryStr.toLocaleUpperCase());
