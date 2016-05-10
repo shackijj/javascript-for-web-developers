e8a483709d17a1278fd75f93b938148d08b9a603
shackijj <shaackiy@gmail.com>
2016-05-10 20:39:28 +0300

    window moves done

--- p8_bom/window-position/js/position.js
+++ p8_bom/window-position/js/position.js
@@ -6,16 +6,25 @@ var topPos = (typeof window.screenRight == "number") ?
 					window.screenRight : window.screenY;
 console.log("leftPos: " + leftPos + " topPos: " + topPos );
 
-var winForMove = window.open('move_me.html', 'winForMove', 'left=100,top=100');
-winForMove.focus();
+var winForMove = window.open('move_me.html', 'winForMove', 'left=100,top=100,width=200,height=200,location=no');
 
 function moveToFunction () {	
 	var inputs = this.form.elements,
 		x = parseInt(inputs["move-to-x"].value),
 		y = parseInt(inputs["move-to-y"].value);
 	winForMove.moveTo(x,y);
+	winForMove.focus();
+};
+
+function moveByFunction () {	
+	var inputs = this.form.elements,
+		x = parseInt(inputs["move-by-x"].value),
+		y = parseInt(inputs["move-by-y"].value);
+	winForMove.moveBy(x,y);
+	winForMove.focus();
 };
 
 var moveToButton = document.getElementById("move-to-btn");
-
 moveToButton.addEventListener("click", moveToFunction);
+var moveByButton = document.getElementById("move-by-btn");
+moveByButton.addEventListener("click", moveByFunction);
