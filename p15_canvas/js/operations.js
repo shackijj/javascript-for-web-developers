583fa479485173c0eecd5110f66696da58bb2b5f
k.shatsky <k.shatsky@netris.ru>
2016-07-01 19:33:13 +0300

    Canvas in process

---
+++ p15_canvas/js/operations.js
@@ -0,0 +1,48 @@
+log_header("Canvas opeations");
+
+var context;
+var rectDrawing = document.getElementById("rectDrawing");
+
+if (rectDrawing.getContext) {
+    context = rectDrawing.getContext("2d");
+    // Line
+    // context.strokeStyle = "red";
+    context.fillStyle = "rgba(0, 0, 255, 0.5";
+    context.fillRect(10, 10, 50, 50);
+    context.fillRect(30, 30, 50, 50);
+    context.clearRect(40, 40, 10, 10);
+
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.strokeStyle = "rgba(0, 0, 0, 0.5)";
+    context.lineWidth = "5";
+
+    context.strokeRect(90, 10, 50, 50);
+    context.strokeRect(110, 30, 50, 50);
+}
+
+var clockDrawing = document.getElementById("clockDrawing");
+
+if (clockDrawing.getContext) {
+    context = clockDrawing.getContext("2d");
+
+    context.beginPath();
+
+    context.arc(100, 100, 99, 0, 2 * Math.PI, false);
+    context.moveTo(194, 100);
+
+    context.arc(100, 100, 94, 0, 2 * Math.PI, false);
+
+    context.moveTo(100, 100);
+    context.lineTo(100, 15);
+
+    context.moveTo(100, 100);
+    context.lineTo(35, 100);
+
+
+
+
+    context.stroke();
+
+
+}
