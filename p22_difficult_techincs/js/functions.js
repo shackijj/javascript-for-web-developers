e1b013bcdbf90f9a28f7c44ba6947e35b867d736
k.shatsky <k.shatsky@netris.ru>
2016-07-27 08:47:45 +0300

    p22 in progress

--- p22_difficult_techincs/js/functions.js
+++ p22_difficult_techincs/js/functions.js
@@ -74,5 +74,138 @@ assert(isArray([1,2]) === true, "Oops, Wrong type detections");
 }());
 
 (function() {
-    
+    // Lazy loading
+
+    var createXHR = (function () {
+        if (typeof XMLHttpRequest != "undefined") {
+            return function() {
+                return new XMLHttpRequest();
+            }
+        } else if (typeof ActiveXObject != "undefined") {
+            return function() {
+                if (typeof arguments.callee.activeXString != "string") {
+                    var versions = ["MSXML.XMLHttp6.0", "MSXML.XMLHttp6.0",
+                        "MSXML.XMLHttp"],
+                        i, len;
+
+                        for (i = 0, len = versions.length; i < len; i++) {
+                            try {
+                                new ActiveXObject(versions[i]);
+                                arguments.callee.activeXString = versions[i];
+                                break;
+                            } catch(ex) {
+                                // nothing
+                            }
+                        }
+
+                        return new ActiveXObject(arguments.callee.activeXString);
+                }
+            };
+        } else {
+            return function() {
+                throw new Error("AjaxUtil.createXHR: No XHR object available");
+            }
+        }
+    }());
+}());
+
+(function() {
+    // Function binding
+
+    function bind(fn, context) {
+        return function() {
+            fn.apply(context, arguments);
+        }
+    }
+
+    var handler = {
+        message: "Event handled",
+
+        handleClick: function(event) {
+            alert(this.message);
+        }
+    };
+
+    var btn = document.getElementById('click_me');
+    /*
+    Closure
+    EventUtil.addHandler(btn, "click", function(event) {
+        handler.handleClick(event);
+    });
+    */
+
+
+    /*
+    ES5
+    EventUtil.addHandler(btn, "click", handler.handleClick.bind(handler));
+    */
+
+    // EventUtil.addHandler(btn, "click", bind(handler.handleClick, handler));
+}());
+
+(function() {
+    function curry(fn) {
+        // Pop first arg and create closure
+        var args = Array.prototype.slice.call(arguments, 1);
+        return function() {
+            var innerArgs = Array.prototype.slice.call(arguments),
+                finalArgs = args.concat(innerArgs);
+            // fn Closure!
+            return fn.apply(null, finalArgs);
+        };
+    }
+
+    function bind(fn, context) {
+        var args = Array.prototype.slice.call(arguments, 2);
+        return function() {
+            var innerArgs = Array.prototype.slice.call(arguments);
+                finalArgs = args.concat(innerArgs);
+            
+            return fn.apply(context, finalArgs);
+        }
+    } 
+
+    function add(num1, num2) {
+        return num1 + num2;
+    }
+
+    var curriedAdd = curry(add, 1);
+    console.log(curriedAdd(3)); // 4
+
+    var handler = {
+        message: "Event handled",
+        clickHandler: function(name, event) {
+            alert(this.message + ":" + name  + ":" + event.type);
+        }
+    };
+
+    var btn = document.getElementById('click_me');
+
+    EventUtil.addHandler(btn, "click", bind(handler.clickHandler, handler, "click_me"));
+}());
+
+(function() {
+    // ES5 only
+    var person = {name: "Nicholas"};
+
+    Object.preventExtensions(person);    
+    assert(Object.isExtensible(person) !== true, "Person must be not extensible");
+    delete person.name;
+    assert(person.name == undefined, "person.name wasn't be deleted");
+
+    var sealedPerson = {name: "Sheby"};
+
+    Object.seal(sealedPerson);
+    sealedPerson.age = 29;
+    delete sealedPerson.name;
+    assert(sealedPerson.name === "Sheby", "sealedPerson.name must be Sheby");
+    assert(sealedPerson.age === undefined, "person is extensibe, but mustn't be");
+
+    var freezedPerson = {name: "Nicholas"};
+    Object.freeze(freezedPerson);
+    freezedPerson.age = 29;
+    assert(freezedPerson.age === undefined, "freezed obj can't be extended");
+    assert(delete freezedPerson.name === false, "freezed obj's props cant be deleted");
+    freezedPerson.name = "Greg";
+    assert(freezedPerson.name === "Nicholas", "freezed obj can't be changed");
 }());
